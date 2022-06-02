38e1cff7a10c284b6b5dec99d5a00603ed3ce443
Arnaud Delobelle <arnaud@geogebra.at>
2022-06-02 09:20:50 +0100

    Add a simple test for AllOfPattern

---
+++ src/test/kotlin/patterns/AllOfPatternTest.kt
@@ -0,0 +1,25 @@
+package patterns
+
+import expressions.RootPath
+import expressions.Subexpression
+import expressions.parsePath
+import expressions.xp
+import org.junit.jupiter.api.Test
+import parser.parseExpression
+import kotlin.test.assertEquals
+
+class AllOfPatternTest {
+
+    @Test
+    fun simpleTest() {
+        val expr = parseExpression("1+x")
+        val n = UnsignedIntegerPattern()
+        val x = FixedPattern(xp("x"))
+        val pattern = allOf(sumContaining(n), sumContaining(x))
+        val matches = pattern.findMatches(Subexpression(RootPath, expr))
+        assertEquals(1, matches.count())
+        val match = matches.first()
+        assertEquals(xp(1), match.getBoundExpr(n))
+        assertEquals(match.getBoundPaths(x), listOf(parsePath("./1")))
+    }
+}
