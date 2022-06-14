e84a97a9c98b65a144ba1f98bf6f3f8bc4e42371
Arnaud Delobelle <arnaud@geogebra.at>
2022-06-14 15:47:20 +0100

    Name constants in Context rate function

--- src/main/kotlin/engine/context/Context.kt
+++ src/main/kotlin/engine/context/Context.kt
@@ -8,13 +8,17 @@ interface Resource {
     val resourceData: ResourceData
 }
 
+private const val BEST_MATCH = 1.0
+private const val DEFAULT_MATCH = 0.5
+private const val WORST_MATCH = 0.0
+
 data class Context(val curriculum: String? = null) {
 
     fun rateResourceData(resourceData: ResourceData): Double {
         return when (curriculum) {
-            resourceData.curriculum -> 1.0
-            null -> 0.5
-            else -> 0.0
+            resourceData.curriculum -> BEST_MATCH
+            null -> DEFAULT_MATCH
+            else -> WORST_MATCH
         }
     }
 
