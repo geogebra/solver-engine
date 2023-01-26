8bc8c0dd7c61970dc5b7b3e52531b07999e066e2
Arnaud Delobelle <arnaud@geogebra.at>
2023-01-26 10:52:23 +0000

    PLUT-446 Delete unused docker image tags

---
+++ scripts/delete_image_tags.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This script deletes docker image tags provided on stdout (one per line) from the solver-engine repository.  It can be
+# used in conjunction with fetch_image_tags.sh to help clean up unused tags.
+
+# A private access token is required in env variable PAT.  Such a token can be obtained from
+#    https://git.geogebra.org/solver-team/solver-engine/-/settings/access_tokens
+
+GITLAB_URL="https://git.geogebra.org"
+PROJECT_ID=102
+REPO_ID=4
+
+if [[ $PAT == "" ]]; then
+  >&2 echo "Env var PAT must be set with a valid private access token for gitlab with scopes api, write_registry"
+  exit 1
+fi
+
+while read -r LINE || [[ -n $LINE ]]; do
+  >&2 echo -n "Deleting tag ${LINE}... "
+  >&2 curl --silent \
+           --request DELETE \
+           --header "PRIVATE-TOKEN: $PAT" \
+           "$GITLAB_URL/api/v4/projects/$PROJECT_ID/registry/repositories/$REPO_ID/tags/${LINE}"
+  >&2 echo
+  sleep 0.1
+done
---
+++ scripts/fetch_image_tags.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# This script outputs to stdout the list of all image tags in the solver engine repository.  It can be used in
+# conjunction with delete_image_tags.sh to help clean up unused tags.
+
+# A private access token is required in env variable PAT.  Such a token can be obtained from
+#    https://git.geogebra.org/solver-team/solver-engine/-/settings/access_tokens
+
+GITLAB_URL="https://git.geogebra.org"
+PROJECT_ID=102
+REPO_ID=4
+
+NEW_TAGS=$(mktemp -t new-tags)
+
+if [[ $PAT == "" ]]; then
+  >&2 echo "Env var PAT must be set with a valid private access token for gitlab with scopes read_api, read_registry"
+  exit 1
+fi
+
+i=1 # For pagination
+while true; do
+  >&2 echo "--- Page $i"
+
+  curl --silent \
+       --header "PRIVATE-TOKEN: $PAT" \
+      "$GITLAB_URL/api/v4/projects/$PROJECT_ID/registry/repositories/$REPO_ID/tags?per_page=100&page=${i}" |
+    jq '.[].name' |
+    sed 's:^.\(.*\).$:\1:' > "$NEW_TAGS"
+
+  if [ -s "$NEW_TAGS" ]; then
+    cat "$NEW_TAGS"
+    i=$((i + 1))
+  else
+    break
+  fi
+done
