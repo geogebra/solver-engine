3c7ed6ba094308b32ddb175835ab1a478322aa7c
Erik Weitnauer <eweitnauer@gmail.com>
2023-05-23 18:59:39 -0400

    fix: try to deploy pre-release SDK package

--- @geogebra/solver-sdk/release.config.cjs
+++ @geogebra/solver-sdk/release.config.cjs
@@ -7,13 +7,17 @@ module.exports = {
     // release new SDK package with @alpha tag when on "main" branch
     { name: 'main', channel: 'alpha' },
     // do a pre-release of SDK package with @plut-xyz tag on "plut-xyz-*" branches
-    { name: 'plut-609-new-sdk-versioning', prerelease: 'plut-609', channel: 'plut-609' },
+    {
+      // this uses [glob notation](https://github.com/micromatch/micromatch#matching-features)
+      name: 'plut-+([0-9])-*',
+      // version a.b.c-plut-xyz.0
+      // this uses [lodash template notation](https://lodash.com/docs/4.17.15#template)
+      prerelease: "${name.replace(/(plut-[0-9]+).*/, '$1')}",
+      // @plut-xyz tag
+      // this uses [lodash template notation](https://lodash.com/docs/4.17.15#template)
+      channel: "${name.replace(/(plut-[0-9]+).*/, '$1')}",
+    },
   ],
-  // {
-  //   "name": "(plut-[0-9]+)*.",
-  //   "prerelease": "${name.replace(/(plut-[0-9]+).*/, '$1')}",
-  //   "channel": "${name.replace(/(plut-[0-9]+).*/, '$1')}"
-  // },
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
