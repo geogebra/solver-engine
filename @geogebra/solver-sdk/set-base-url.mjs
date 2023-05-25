893493dcc0888cd8910abe5b772208b48fbeaa4d
Erik Weitnauer <eweitnauer@gmail.com>
2023-05-25 13:33:42 -0400

    fix: 11th try

---
+++ @geogebra/solver-sdk/set-base-url.mjs
@@ -0,0 +1,20 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { argv } from 'node:process';
+
+const baseUrl = argv[2];
+
+if (!baseUrl) {
+  console.error('Error: Need to pass API url for the SDK as an argument to this script.');
+} else {
+  const __dirname = fileURLToPath(path.dirname(import.meta.url));
+  // do this, but use node instead
+  const apiPath = path.join(__dirname, './src/api.ts');
+  const api = fs.readFileSync(apiPath, 'utf8');
+  const newApi = api.replace(
+    /const BASE_URL = .*/,
+    `const BASE_URL = '${baseUrl}';`,
+  );
+  fs.writeFileSync(apiPath, newApi, 'utf8');
+}
