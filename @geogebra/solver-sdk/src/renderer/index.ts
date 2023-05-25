4dd453a38205e701e79dec06a31f9d5fe5d0a3ed
Arnaud Delobelle <arnaud@geogebra.at>
2023-05-25 16:34:05 +0100

    Fix build error

--- @geogebra/solver-sdk/src/renderer/index.ts
+++ @geogebra/solver-sdk/src/renderer/index.ts
@@ -1,5 +1,5 @@
-export type { LatexSettings } from './tree-to-latex';
-export { LatexTransformer, treeToLatex } from './tree-to-latex';
+export type { LatexSettings, LatexTransformer } from './tree-to-latex';
+export { treeToLatex } from './tree-to-latex';
 export { setsSolutionFormatter, simpleSolutionFormatter } from './solution-formatter';
 export { treeToSolver } from './tree-to-solver';
 export { specialTransformationLatex } from './transformation-to-latex';
