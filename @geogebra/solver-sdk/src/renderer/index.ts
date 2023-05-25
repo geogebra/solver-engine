d668e01b359cca3642f0fac2fd5b222f7c15489d
Arnaud Delobelle <arnaud@geogebra.at>
2023-05-25 16:22:34 +0100

    Add specialTransformationLatex that can render equation addition and use it in poker

--- @geogebra/solver-sdk/src/renderer/index.ts
+++ @geogebra/solver-sdk/src/renderer/index.ts
@@ -2,3 +2,4 @@ export type { LatexSettings } from './tree-to-latex';
 export { LatexTransformer, treeToLatex } from './tree-to-latex';
 export { setsSolutionFormatter, simpleSolutionFormatter } from './solution-formatter';
 export { treeToSolver } from './tree-to-solver';
+export { specialTransformationLatex } from './transformation-to-latex';
