083762f2bc4650ede105903e06acfa4c80fb51b9
Arnaud Delobelle <arnaud@geogebra.at>
2023-07-21 14:32:58 +0100

    Add a fallback plan for quadratics with negative discriminant

--- engine/src/main/kotlin/engine/methods/TaskSet.kt
+++ engine/src/main/kotlin/engine/methods/TaskSet.kt
@@ -14,7 +14,8 @@ class TaskSet(
     private val explanationMaker: MetadataMaker,
     private val skillMakers: List<MetadataMaker> = emptyList(),
     private val tasksProducer: TasksProducer,
-) : CompositeMethod() {
+    specificPlans: List<Method> = emptyList(),
+) : CompositeMethod(specificPlans) {
     override fun run(ctx: Context, sub: Expression): Transformation? {
         for (match in pattern.findMatches(ctx, RootMatch, sub)) {
             val tasks = tasksProducer.produceTasks(ctx, sub, match)
@@ -52,6 +53,7 @@ class TaskSetBuilder : CompositeMethodBuilder() {
             explanationMaker = explanationMaker,
             skillMakers = skillMakers,
             tasksProducer = tasksProducer,
+            specificPlans = specificPlans,
         )
     }
 }
--- methods/src/main/kotlin/methods/fallback/FallbackPlans.kt
+++ methods/src/main/kotlin/methods/fallback/FallbackPlans.kt
@@ -1,12 +1,24 @@
 package methods.fallback
 
+import engine.expressions.Constants
+import engine.expressions.Identity
+import engine.expressions.VoidExpression
+import engine.expressions.negOf
+import engine.expressions.powerOf
+import engine.expressions.productOf
+import engine.expressions.sumOf
 import engine.methods.CompositeMethod
 import engine.methods.PublicMethod
 import engine.methods.RunnerMethod
 import engine.methods.plan
+import engine.methods.taskSet
+import engine.patterns.ArbitraryVariablePattern
+import engine.patterns.QuadraticPolynomialPattern
+import engine.steps.metadata.metadata
 import methods.approximation.ApproximationPlans
 import methods.constantexpressions.ConstantExpressionsPlans
 import methods.factor.FactorPlans
+import methods.inequalities.solveConstantInequalitySteps
 import methods.integerarithmetic.IntegerArithmeticPlans
 import methods.polynomials.PolynomialsPlans
 
@@ -16,6 +28,7 @@ enum class FallbackPlans(override val runner: CompositeMethod) : RunnerMethod {
     @PublicMethod
     ExpressionIsFullySimplified(
         plan {
+
             specificPlans(
                 ConstantExpressionsPlans.SimplifyConstantExpression,
                 IntegerArithmeticPlans.EvaluateArithmeticExpression,
@@ -28,6 +41,53 @@ enum class FallbackPlans(override val runner: CompositeMethod) : RunnerMethod {
 
             steps {
                 apply(FallbackRules.ExpressionIsFullySimplified)
+            }
+        },
+    ),
+
+    // Users shouldn't call this method
+    @PublicMethod
+    QuadraticHasNegativeDiscriminant(
+        taskSet {
+
+            specificPlans(
+                PolynomialsPlans.SimplifyAlgebraicExpressionInOneVariable,
+                FactorPlans.FactorPolynomialInOneVariable,
+            )
+
+            explanation = Explanation.QuadraticIsIrreducible
+            val quadratic = QuadraticPolynomialPattern(ArbitraryVariablePattern())
+            pattern = quadratic
+
+            tasks {
+                val a = get(quadratic::quadraticCoefficient)!!
+                val b = get(quadratic::linearCoefficient)!!
+                val c = get(quadratic::constantTerm)!!
+
+                val discriminant = sumOf(
+                    powerOf(b, Constants.Two),
+                    negOf(
+                        productOf(Constants.Four, a, c),
+                    ),
+                )
+
+                val checkDiscriminantIsNegative = task(
+                    context = context.copy(solutionVariables = emptyList()),
+                    startExpr = engine.expressions.lessThanOf(discriminant, Constants.Zero),
+                    explanation = metadata(Explanation.CheckDiscriminantIsNegative),
+                    stepsProducer = solveConstantInequalitySteps,
+                ) ?: return@tasks null
+
+                if (checkDiscriminantIsNegative.result !is Identity) {
+                    return@tasks null
+                }
+
+                task(
+                    startExpr = VoidExpression(),
+                    explanation = metadata(Explanation.QuadraticIsIrreducibleBecauseDiscriminantIsNegative),
+                )
+
+                allTasks()
             }
         },
     ),
